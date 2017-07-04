3f8df681c493296d8a61776e4f0fdafe09ecf8e4
bibou <pmestivier@club.fr>
2017-07-04 20:49:07 +0200

    final

--- src/fcm/PushNotifictionHelper.java
+++ src/fcm/PushNotifictionHelper.java
@@ -7,6 +7,7 @@ import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -22,27 +23,28 @@ import com.google.gson.JsonArray;
 public class PushNotifictionHelper {
 	public final static String AUTH_KEY_FCM = ServeurMethodes.key_gcm;
 	public final static String API_URL_FCM = "https://fcm.googleapis.com/fcm/send";
-	public static  Map<Integer,String> mTypeActivite;
+	public static Map<Integer, String> mTypeActivite;
 	static {
-	mTypeActivite=new HashMap<Integer,String>();
+		mTypeActivite = new HashMap<Integer, String>();
 
-	mTypeActivite.put(1,"ic_barrestorond");
-	mTypeActivite.put(2,"ic_sportrnd");
-	mTypeActivite.put(3,"ic_expositionrnd");
-	mTypeActivite.put(4,"ic_jeurnd");
-	mTypeActivite.put(5,"ic_friendsrnd");
-	mTypeActivite.put(6,"ic_suggestionwayd");
-	mTypeActivite.put(7,"ic_entraide");
-	mTypeActivite.put(8,"ic_entraide");
-	
-	
+		mTypeActivite.put(1, "ic_barrestorond");
+		mTypeActivite.put(2, "ic_sportrnd");
+		mTypeActivite.put(3, "ic_expositionrnd");
+		mTypeActivite.put(4, "ic_jeurnd");
+		mTypeActivite.put(5, "ic_friendsrnd");
+		mTypeActivite.put(6, "ic_suggestionwayd");
+		mTypeActivite.put(7, "ic_entraide");
+		mTypeActivite.put(8, "ic_entraide");
+
 	}
-	
-	public synchronized static String sendPushNotificationSuggestionList(ArrayList<Personne> listpersonne,Activite activite)
+
+	public synchronized static String sendPushNotificationSuggestionList(
+			ArrayList<Personne> listpersonne, Activite activite)
 			throws IOException {
-		
-		ArrayList<String> listpersonneGcm=ServeurMethodes.getListGCM(listpersonne);
-		
+
+		ArrayList<String> listpersonneGcm = ServeurMethodes
+				.getListGCM(listpersonne);
+
 		String result = "";
 		URL url = new URL(API_URL_FCM);
 		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
@@ -56,21 +58,25 @@ public class PushNotifictionHelper {
 		conn.setRequestProperty("Content-Type", "application/json");
 
 		JSONObject json = new JSONObject();
-		json.put("registration_ids",listpersonneGcm);
+		json.put("registration_ids", listpersonneGcm);
 		JSONObject info = new JSONObject();
-		info.put("title",activite.getTitre()); // Notification title
+		info.put("title", activite.getTitre()); // Notification title
 		info.put("body", StringEscapeUtils.unescapeJava(activite.getLibelle())); // Notification
 		info.put("click_action", "notificationSuggestion");
-		info.put("icon",mTypeActivite.get(activite.getTypeactivite()));
+		info.put("icon", mTypeActivite.get(activite.getTypeactivite()));
 		json.put("notification", info);
-		
+
 		JSONObject data = new JSONObject();
-	
+
 		data.put("idactiviteFromNotification", activite.getId());
-		data.put("idtypeactivite",mTypeActivite.get(activite.getTypeactivite()));
-		data.put("time_to_live",300);
+		data.put("idtypeactivite",
+				mTypeActivite.get(activite.getTypeactivite()));
+		int dureeNotification = (int) (activite.datefin.getTime() - new Date()
+				.getTime()) / 1000;
+
+		System.out.println("dureeenot"+dureeNotification);
+		data.put("time_to_live", dureeNotification);
 		json.put("data", data); // Notification
-	
 
 		try {
 			OutputStreamWriter wr = new OutputStreamWriter(
@@ -96,8 +102,6 @@ public class PushNotifictionHelper {
 		return result;
 
 	}
-	
-	
 
 	public static String sendPushNotificationTo(String deviceToken)
 			throws IOException {
@@ -114,10 +118,10 @@ public class PushNotifictionHelper {
 		conn.setRequestProperty("Content-Type", "application/json");
 
 		JSONObject json = new JSONObject();
-		List< String> test = new ArrayList<String>();
+		List<String> test = new ArrayList<String>();
 		test.add(deviceToken);
-		
-		json.put("to",deviceToken.trim());
+
+		json.put("to", deviceToken.trim());
 		JSONObject info = new JSONObject();
 		info.put("title", "notification title"); // Notification title
 		info.put("body", "message body"); // Notification
