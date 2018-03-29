b28b702c279eca6f58d24c95fae00c5755c9468a
philippe esi <pmestivier@club.fr>
2018-03-29 15:04:04 +0200

    ok

--- src/texthtml/pro/CommunText.java
+++ src/texthtml/pro/CommunText.java
@@ -5,38 +5,38 @@ public class CommunText {
 	
 	public static final String LABEL_BUTTON_PROPOSEZ = "Proposez";
 	public static final String INFO_SUPPRIME_SELECTION ="Supprimez les selections";
-	public static int TAILLE_TITRE_ACTIVITE_MAX = 200;
+	public static final  int TAILLE_TITRE_ACTIVITE_MAX = 200;
 	
-	public static int TAILLE_DESCRIPTION_ACTIVITE_MAX = 2000;
-	public static int TAILLE_ADRESSE_MAX = 250;
-	public static String HINT_ADRESSE="Saissisez une adresse";
-	public static String HINT_TITRE="Saisisez un titre";
-	public static String HINT_TELEPHONE="XXXXXXXXXX";
+	public static final int TAILLE_DESCRIPTION_ACTIVITE_MAX = 2000;
+	public static final int TAILLE_ADRESSE_MAX = 250;
+	public static final String HINT_ADRESSE="Saissisez une adresse";
+	public static final String HINT_TITRE="Saisisez un titre";
+	public static final String HINT_TELEPHONE="XXXXXXXXXX";
 	public static final String HINT_MOT_DE_PASSE = "Saisissez le mot de passe";
 	public static final String HINT_MOT_DE_PASSE_BIS = "Ressaisir le mot de passe";
 	public static final String HINT_SIRET = "Numéro de  Siret (14 chiffres) ";
 	public static final String HINT_EMAIL = "Adresse email";
 	public static final int NBR_ACTIVITE_MAX = 5;
 
-	public static int TAILLE_PSEUDO_MAX = 40;
-	public static int TAILLE_SITE_WEB_MAX = 100;
-	public static int TAILLE_TELEPHONNE_MAX = 10;
-	public static int TAILLE_SIRET_MAX = 14;
-	public static int TAILLE_DESCRIPTION_PROFIL_MAX = 250;
+	public static final int TAILLE_PSEUDO_MAX = 40;
+	public static final int TAILLE_SITE_WEB_MAX = 100;
+	public static final int TAILLE_TELEPHONNE_MAX = 10;
+	public static final int TAILLE_SIRET_MAX = 14;
+	public static final int TAILLE_DESCRIPTION_PROFIL_MAX = 250;
 	
 	
-	public static String LABEL_TELEPHONE = "Téléphone *";
-	public static String LABEL_NOM = "Pseudo *";
-	public static String LABEL_SITE_WEB = "Site Web";
-	public static String LABEL_NUMERO_SIRET = "Numéro SIRET *";
-	public static String LABEL_ADRESSE = "Adresse *";
-	public static String LABEL_DESCRIPTION_PROFIL = "Description";
+	public static final String LABEL_TELEPHONE = "Téléphone *";
+	public static final String LABEL_NOM = "Pseudo *";
+	public static final String LABEL_SITE_WEB = "Site Web";
+	public static final String LABEL_NUMERO_SIRET = "Numéro SIRET *";
+	public static final String LABEL_ADRESSE = "Adresse *";
+	public static final String LABEL_DESCRIPTION_PROFIL = "Description";
 	
-	public static String LABEL_DATE_DEBUT = "Date début";
-	public static String LABEL_DATE_FIN = "Date fin";
-	public static String LABEL_TYPE_ACTIVITE = "Type activité";
-	public static String LABEL_DESCRIPTION_ACTIVITE = "Description";
-	public static String LABEL_TITRE = "Titre";
+	public static final String LABEL_DATE_DEBUT = "Date début";
+	public static final String LABEL_DATE_FIN = "Date fin";
+	public static final String LABEL_TYPE_ACTIVITE = "Type activité";
+	public static final String LABEL_DESCRIPTION_ACTIVITE = "Description";
+	public static final String LABEL_TITRE = "Titre";
 	
 	public static String getHintDescriptionProfil() {
 
--- src/texthtml/pro/CompteProText.java
+++ src/texthtml/pro/CompteProText.java
@@ -2,31 +2,31 @@ package texthtml.pro;
 
 public class CompteProText {
 	
-	public static String HINT_SITEWEB="http://monsite.fr";
-	public static String HINT_SIRET=CommunText.HINT_SIRET;
-	public static String HINT_ADRESSE="Saissisez l'adresse de la structure.";
-	public static String HINT_TELEPHONE=CommunText.HINT_TELEPHONE;
-	public static String AUCUN_FICHIER_SELECTIONNE="Aucune image selectionnée";
+	public static final String HINT_SITEWEB="http://monsite.fr";
+	public static final String HINT_SIRET=CommunText.HINT_SIRET;
+	public static final String HINT_ADRESSE="Saissisez l'adresse de la structure.";
+	public static final String HINT_TELEPHONE=CommunText.HINT_TELEPHONE;
+	public static final String AUCUN_FICHIER_SELECTIONNE="Aucune image selectionnée";
 	
-	public static String TYPE_COMPTE="Type de compte";
+	public static final String TYPE_COMPTE="Type de compte";
 	
-	public static int TAILLE_PSEUDO_MAX = CommunText.TAILLE_PSEUDO_MAX;
-	public static int TAILLE_SITE_WEB_MAX = CommunText.TAILLE_SITE_WEB_MAX;
-	public static int TAILLE_TELEPHONNE_MAX = CommunText.TAILLE_TELEPHONNE_MAX;
-	public static int TAILLE_SIRET_MAX = CommunText.TAILLE_SIRET_MAX;
-	public static int TAILLE_ADRESSE_MAX = CommunText.TAILLE_ADRESSE_MAX;
-	public static int TAILLE_DESCRIPTION_PROFIL_MAX = CommunText.TAILLE_DESCRIPTION_PROFIL_MAX;
+	public static final int TAILLE_PSEUDO_MAX = CommunText.TAILLE_PSEUDO_MAX;
+	public static final int TAILLE_SITE_WEB_MAX = CommunText.TAILLE_SITE_WEB_MAX;
+	public static final int TAILLE_TELEPHONNE_MAX = CommunText.TAILLE_TELEPHONNE_MAX;
+	public static final int TAILLE_SIRET_MAX = CommunText.TAILLE_SIRET_MAX;
+	public static final int TAILLE_ADRESSE_MAX = CommunText.TAILLE_ADRESSE_MAX;
+	public static final int TAILLE_DESCRIPTION_PROFIL_MAX = CommunText.TAILLE_DESCRIPTION_PROFIL_MAX;
 
 	
-	public static String TITRE_ONGLET = "Mon compte";
+	public static final String TITRE_ONGLET = "Mon compte";
 
-	public static String TITRE_JUMBO = "Votre compte";
-	public static String MESSAGE_JUMBO_L1 = "Renseigner votre profil, décrivez votre activité.";
+	public static final String TITRE_JUMBO = "Votre compte";
+	public static final String MESSAGE_JUMBO_L1 = "Renseigner votre profil, décrivez votre activité.";
 			
-	public static String MESSAGE_JUMBO_L2 = "Préciser votre adresse exacte pour être localisé par la communauté.";
+	public static final String MESSAGE_JUMBO_L2 = "Préciser votre adresse exacte pour être localisé par la communauté.";
 	
 	
-	public static String LABEL_NOM = CommunText.LABEL_NOM;
+	public static final String LABEL_NOM = CommunText.LABEL_NOM;
 	
 	public static String LABEL_SITE_WEB = CommunText.LABEL_SITE_WEB;
 	public static String LABEL_NUMERO_SIRET = CommunText.LABEL_NUMERO_SIRET;
